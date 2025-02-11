70688739ef7ca7c59611cae69376e0895dc6ee40
riyan <riyan@RAR>
2025-02-11 18:21:31 +0000

    Add project files.

---
+++ rubiks cube/Form1.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace rubiks_cube
+{
+    partial class test1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // test1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "test1";
+            this.Text = "test1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
